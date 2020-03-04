c16bf98e26a2ba4485d6c4dc9fa963f56583cae5
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2020-03-05 03:06:12 +0900

    Split mnist files

--- chap3/neuralnet_3.6.2.py
+++ chap3/neuralnet_3.6.2.py
@@ -3,7 +3,7 @@ import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 import pickle
-from mnist import load_mnist
+from dataset.mnist import load_mnist
 from activation_funcs import sigmoid, softmax
 
 
